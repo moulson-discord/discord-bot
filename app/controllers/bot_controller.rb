bbe2b0044971b8356fb44983148d7974804591f0
Daniel Moulson <dan24797@gmail.com>
2017-07-09 15:57:19 +0100

    bot is no longer immortal, can now also obey commands

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,12 +1,10 @@
 class BotController < ApplicationController
 	def start
-		system "rake start_bot"
+		call_rake :start_bot
 		flash[:notice] = "Bot is running"
 	end
 
 	def stop
-		flash[:notice] = 'Stopping bot'
-		lines = (`ps aux | grep 'rake start_bot'`).split(/\n/)
-		debugger
+
 	end
 end
--- app/models/bot.rb
+++ app/models/bot.rb
@@ -6,8 +6,17 @@ class Bot < ApplicationRecord
 	def self.start
 		bot = Discordrb::Commands::CommandBot.new token: configatron.token, client_id: 217444617899999232, prefix: '!'
 
+		bot.command :d20 do |event|
+			rand(1,20).to_s
+		end
 		bot.message(with_text: 'Ping!') do |event|
 	  		event.respond 'Pong!'
+		end
+		bot.command(:exit, help_available: false) do |event|
+		  break unless event.user.id == 137281612818677761 #Only I can do this
+
+		  bot.send_message(event.channel.id, 'Bot is shutting down')
+		  exit
 		end
 		bot.run
 	end
