3cb1d2d9f3922407c68f433ff409b5d903bc8369
Daniel Moulson <dan24797@gmail.com>
2017-07-09 21:59:34 +0100

    sound works now, cleaned up some commands

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -7,18 +7,19 @@ class Bot < ApplicationRecord
 		bot = Discordrb::Commands::CommandBot.new token: configatron.token, client_id: 217444617899999232, prefix: '!'
 
 		bot.command :d20 do |event|
-			rand(1,20).to_s
+			r = Random.new
+			bot.send_message(event.channel.id, r.rand(1...20).to_s)
 		end
-		bot.message(with_text: 'Ping!') do |event|
+		bot.command(:ping) do |event|
 	  		event.respond 'Pong!'
 		end
-		bot.command(:exit, help_available: false) do |event|
+		bot.command(:stop, help_available: false) do |event|
 		  break unless event.user.id == 137281612818677761 #Only I can do this
 
 		  bot.send_message(event.channel.id, 'Bot is shutting down')
 		  exit
 		end
-		bot.command(:voice_join) do |event|
+		bot.command(:summon) do |event|
 		  # The `voice_channel` method returns the voice channel the user is currently in, or `nil` if the user is not in a
 		  # voice channel.
 		  channel = event.user.voice_channel
@@ -32,7 +33,7 @@ class Bot < ApplicationRecord
 		# A simple command that plays back an mp3 file.
 		bot.command(:play) do |event|
 		  voice_bot = event.voice
-		  event.respond("I'd play a file now if one existed")
+		  voice_bot.play_file("http://freesound.org/data/previews/397/397235_1648170-lq.mp3")
 		end
 		bot.run
 	end
