f491eb64fe0dbf0a3b7ae2e1a880dae2e1c9bace
Daniel Moulson <dan24797@gmail.com>
2017-07-09 16:05:41 +0100

    bot now joins a voice channel

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -18,6 +18,22 @@ class Bot < ApplicationRecord
 		  bot.send_message(event.channel.id, 'Bot is shutting down')
 		  exit
 		end
+		bot.command(:voice_join) do |event|
+		  # The `voice_channel` method returns the voice channel the user is currently in, or `nil` if the user is not in a
+		  # voice channel.
+		  channel = event.user.voice_channel
+
+		  next "no channel" unless channel
+
+		  bot.voice_connect(channel)
+		  "Connected to voice channel: #{channel.name}"
+		end
+
+		# A simple command that plays back an mp3 file.
+		bot.command(:play) do |event|
+		  voice_bot = event.voice
+		  event.respond("I'd play a file now if one existed")
+		end
 		bot.run
 	end
 end
